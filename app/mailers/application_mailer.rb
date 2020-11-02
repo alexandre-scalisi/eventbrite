90c1b421e761ea1d7ec02fc8c76a815014a7338c
alexlemia13 <alexlemia13@outlook.fr>
2020-11-02 15:52:14 +0100

    second commit

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'alexlemia13@outlook.fr'
   layout 'mailer'
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ApplicationRecord
+  after_create :welcome_send
+
+  
   has_many :attendances
   has_many :attended_events, through: :attendances, source: :event
   has_many :administered_events, through: :attendances, source: :event
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
 end
